93082ca23ff23a2bd7e959b1a5c6fa232c35b6b0
grelamarina <grelamarina22@gmail.com>
2023-03-09 19:54:07 -0300

    tercer entrega

--- main.js
+++ main.js
@@ -35,11 +35,11 @@ const mostrarProductos = () => {
         card.classList.add("col-xl-3","col-md-6","col-xs-12");
         card.innerHTML = `
             <div class="card">
-                <img src="${Producto.img}" class="card-img-top imgProductos" alt="${Producto.nombre}">
+                <img src="${producto.img}" class="card-img-top imgProductos" alt="${Producto.nombre}">
                 <div class="card-body">
-                <h3 class="card-title"> ${Producto.nombre}</h3>
-                <p class="card-text"> ${Producto.precio} </p>
-                <button class="btn colorBoton" id="boton${Producto.id}"> Agregar compra</button>
+                <h3 class="card-title"> ${producto.nombre}</h3>
+                <p class="card-text"> ${producto.precio} </p>
+                <button class="btn colorBoton" id="boton${producto.id}"> Agregar compra</button>
                 </div>  
             </div>
         `
@@ -66,31 +66,59 @@ const contenedorCarrito = document.getElementById("contenedorCarrito");
 
 const verCarrito = document.getElementById("verCarrito");
 verCarrito.addEventListener("click", () => {
-    mostrarCarrito();
+   mostrarCarrito();
     
 });
 
 const mostrarCarrito = () => {
     contenedorCarrito.innerHTML="";
-    carrito.forEach((prodcuto) => {
+    carrito.forEach((producto) => {
         const card = document.createElement("div");
         card.classList.add("col-xl-3","col-md-6","col-xs-12");
         card.innerHTML = `
             <div class="card">
-                <img src="${Producto.img}" class="card-img-top imgProductos" alt="${Producto.nombre}">
+                <img src="${producto.img}" class="card-img-top imgProductos" alt="${Producto.nombre}">
                 <div class="card-body">
-                <h3 class="card-title"> ${Producto.nombre}</h3>
-                <p class="card-text"> ${Producto.precio} </p>
-                <button class="btn colorBoton" id="eliminar${Producto.id}"> Agregar compra</button>
+                <h3 class="card-title"> ${producto.nombre}</h3>
+                <p class="card-text"> ${producto.precio} </p>
+                <button class="btn colorBoton" id="eliminar${producto.id}"> Eliminar Producto</button>
                 </div>  
             </div>
         `
         contenedorCarrito.appendChild(card);
 
-        const boton = document.getElementById(`elinar${producto.id}`);
+        const boton = document.getElementById(`eliminar${producto.id}`);
         boton.addEventListener("click", () => {
-            eliminarDelCarrito(prodcuto.id);
+            eliminarDelCarrito(producto.id);
         })
     })
 }
-    
+
+const eliminarDelCarrito = (id) => {
+    const producto = carrito.find ((producto) => producto.id === id);
+    const indice = carrito.indexOf (producto);
+    carrito.splice (indice, 1);
+    mostrarCarrito();
+}
+
+const vaciarCarrito = document.getElementById("vaciarCarrito");
+
+vaciarCarrito.addEventListener("click", () => {
+    eliminarTodoElCarrito();
+
+})
+
+const eliminarTodoElCarrito = () => {
+    carrito = [];
+    mostrarCarrito();
+}
+
+const total = document.getElementById("total");
+const calcularTodo = () => {
+    let totalCompra = 0;
+    carrito.forEach((producto) => {
+        totalCompra += producto.precio * producto.cantidad;
+        
+    })
+    total.innerHTML = `Total: $${totalCompra}`;
+}
