8cf1c98cbcb5fd834c4646cd9bceb45d4bb1d620
marina <grelamarina22@gmail.com>
2023-03-09 22:19:25 -0300

    modificando

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
 class Producto {
-    constructor(id, nombre, precio, img){
-        this.id = id;
-        this.nombre = nombre;
-        this.precio = precio;
-        this.img = img;
-        this.cantidad = 1;
-    }
+  constructor(id, nombre, precio, img) {
+    this.id = id;
+    this.nombre = nombre;
+    this.precio = precio;
+    this.img = img;
+    this.cantidad = 1;
+  }
 }
 
-const open = new Producto(1, "Open", 202, "img/open.jpg");
+const open = new Producto(1, "Open", 202, "img/open.png");
 const advanced = new Producto(2, "Advance", 190, "img/advance.jpg");
-const rescue = new Producto(3, "Rescue", 196, "img/recue.jpg");
+const rescue = new Producto(3, "Rescue", 196, "img/rescue.png");
 const emergency = new Producto(4, "Emergency", 100, "img/emergency.jpg");
 const nocturno = new Producto(5, "Nocturno", 129, "img/night.jpg");
-const equipamiento = new Producto(6, "Equipamiento", 58, "img/equipamiento.jpg");
-const scuba = new Producto(7, "Scuba", 125, "img/scuba.jpg");
-const master = new Producto(8, "Master", 200, "img/maste.jpg");  
+const equipamiento = new Producto(
+  6,
+  "Equipamiento",
+  58,
+  "img/equipamiento.jpg"
+);
+const scuba = new Producto(7, "Scuba", 125, "img/scuba.png");
+const master = new Producto(8, "Master", 200, "img/master.jpg");
 const asistente = new Producto(9, "Asistente", 160, "img/asistente.jpg");
 const apneista = new Producto(10, "Apneísta", 150, "img/apneista/jpg");
 const cavernas = new Producto(11, "Cavernas", 150, "img/cavernas.jpg");
@@ -23,17 +28,31 @@ const hielo = new Producto(12, "Hielo", 190, "img/hielo.jpg");
 const director = new Producto(13, "Director", 220, "img/director.jpg");
 const arrecife = new Producto(14, "Arrecife", 170, "img/arricife.jpg");
 
-const productos = [open, advanced, rescue, emergency, nocturno, equipamiento, scuba, master, asistente, cavernas, hielo, director, arrecife];
+const productos = [
+  open,
+  advanced,
+  rescue,
+  emergency,
+  nocturno,
+  equipamiento,
+  scuba,
+  master,
+  asistente,
+  cavernas,
+  hielo,
+  director,
+  arrecife,
+];
 
 let carrito = [];
 
 const contenedorProductos = document.getElementById("contenedorProductos");
 
 const mostrarProductos = () => {
-    productos.forEach((producto) => {
-        const card = document.createElement("div");
-        card.classList.add("col-xl-3","col-md-6","col-xs-12");
-        card.innerHTML = `
+  productos.forEach((producto) => {
+    const card = document.createElement("div");
+    card.classList.add("col-xl-3", "col-md-6", "col-xs-12");
+    card.innerHTML = `
             <div class="card">
                 <img src="${producto.img}" class="card-img-top imgProductos" alt="${Producto.nombre}">
                 <div class="card-body">
@@ -43,21 +62,22 @@ const mostrarProductos = () => {
                 </div>  
             </div>
         `
-        contenedorProductos.appendChild(card);
-        const boton = document.getElementById(`boton${producto.id}`);
-        boton.addEventListener("click", () => {
-        })
-    })
-}
+    contenedorProductos.appendChild(card);
+    const boton = document.getElementById(`boton${producto.id}`);
+    boton.addEventListener("click", () => {
+        agregarAlcarrito(producto.id)
+    });
+  });
+};
 
 const agregarAlcarrito = (id) => {
-    const producto = productos.find ((producto) => producto.id === id);
-    const productoEnCarrito = carrito.find ((producto) => producto.id === id);
-    if(productoEnCarrito){
-        productoEnCarrito.cantidad++;        
-    }else {
-        carrito.push(producto);
-    }
+  const producto = productos.find((producto) => producto.id === id);
+  const productoEnCarrito = carrito.find((producto) => producto.id === id);
+  if (productoEnCarrito) {
+    productoEnCarrito.cantidad++;
+  } else {
+    carrito.push(producto);
+  }
 }
 
 mostrarProductos();
@@ -66,16 +86,15 @@ const contenedorCarrito = document.getElementById("contenedorCarrito");
 
 const verCarrito = document.getElementById("verCarrito");
 verCarrito.addEventListener("click", () => {
-   mostrarCarrito();
-    
+  mostrarCarrito();
 });
 
 const mostrarCarrito = () => {
-    contenedorCarrito.innerHTML="";
-    carrito.forEach((producto) => {
-        const card = document.createElement("div");
-        card.classList.add("col-xl-3","col-md-6","col-xs-12");
-        card.innerHTML = `
+  contenedorCarrito.innerHTML = "";
+  carrito.forEach((producto) => {
+    const card = document.createElement("div");
+    card.classList.add("col-xl-3", "col-md-6", "col-xs-12");
+    card.innerHTML = `
             <div class="card">
                 <img src="${producto.img}" class="card-img-top imgProductos" alt="${Producto.nombre}">
                 <div class="card-body">
@@ -84,41 +103,39 @@ const mostrarCarrito = () => {
                 <button class="btn colorBoton" id="eliminar${producto.id}"> Eliminar Producto</button>
                 </div>  
             </div>
-        `
-        contenedorCarrito.appendChild(card);
+        `;
+    contenedorCarrito.appendChild(card);
 
-        const boton = document.getElementById(`eliminar${producto.id}`);
-        boton.addEventListener("click", () => {
-            eliminarDelCarrito(producto.id);
-        })
-    })
-}
+    const boton = document.getElementById(`eliminar${producto.id}`);
+    boton.addEventListener("click", () => {
+      eliminarDelCarrito(producto.id);
+    });
+  });
+};
 
 const eliminarDelCarrito = (id) => {
-    const producto = carrito.find ((producto) => producto.id === id);
-    const indice = carrito.indexOf (producto);
-    carrito.splice (indice, 1);
-    mostrarCarrito();
-}
+  const producto = carrito.find((producto) => producto.id === id);
+  const indice = carrito.indexOf(producto);
+  carrito.splice(indice, 1);
+  mostrarCarrito();
+};
 
 const vaciarCarrito = document.getElementById("vaciarCarrito");
 
 vaciarCarrito.addEventListener("click", () => {
-    eliminarTodoElCarrito();
-
-})
+  eliminarTodoElCarrito();
+});
 
 const eliminarTodoElCarrito = () => {
-    carrito = [];
-    mostrarCarrito();
-}
+  carrito = [];
+  mostrarCarrito();
+};
 
 const total = document.getElementById("total");
 const calcularTodo = () => {
-    let totalCompra = 0;
-    carrito.forEach((producto) => {
-        totalCompra += producto.precio * producto.cantidad;
-        
-    })
-    total.innerHTML = `Total: $${totalCompra}`;
-}
+  let totalCompra = 0;
+  carrito.forEach((producto) => {
+    totalCompra += producto.precio * producto.cantidad;
+  });
+  total.innerHTML = `Total: $${totalCompra}`;
+};
